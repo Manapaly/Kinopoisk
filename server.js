d4f4ee1e32582667bf12c82c5afb4762ed8fc515
Manapaly <ernat3505@gmail.com>
2023-05-19 20:37:27 +0600

    session

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const session = require('express-session');
+const mongooseStore = require('connect-mongo')
+
 const app = express();
 
 require('./server/config/db')
+
 app.use(express.static(__dirname + '/public'))
 app.set('view engine', "ejs")
+
+app.use(session({
+    name: 'kinopoisk.session',
+    secret: 'keyboard cat',
+    maxAge: 1000 * 60 * 60 * 7,
+    resave: false,
+    store: mongooseStore.create({
+        mongoUrl: 'mongodb://127.0.0.1:27017'
+    })
+}))
 
 app.use(express.urlencoded())
 
